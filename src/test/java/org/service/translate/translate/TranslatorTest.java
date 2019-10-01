ebcffafc663b0de5906ca1c60504fe156a7b2ee7
Twayn <Twayn@ya.ru>
2019-10-01 09:57:46 +0500

    Some refactoring.

--- src/test/java/org/service/translate/translate/TranslatorTest.java
+++ src/test/java/org/service/translate/translate/TranslatorTest.java
@@ -3,12 +3,13 @@ package org.service.translate.translate;
 import static com.google.cloud.translate.Translate.TranslateOption.sourceLanguage;
 import static com.google.cloud.translate.Translate.TranslateOption.targetLanguage;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Map;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.service.translate.translate.split.Splitter;
@@ -20,54 +21,51 @@ import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.Translation;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TranslatorTest {
 	@Autowired
 	Splitter splitter;
 
-	private static Translation fish;
-	private static Translation replay;
-	private static Translation song;
+	private Map<String, String> enRuTranslations =
+			Map.of(
+					"fish", "рыба"
+			);
 
-	@BeforeClass
-	public static void setup() {
-		fish = translationMock("рыба");
-		replay = translationMock("replay");
-		song = translationMock("songs");
-	}
+	private Map<String, String> ruEnTranslations =
+			Map.of(
+					"повтор", "replay",
+					"песни", "songs"
+			);
 
 	@Test
-	public void enRuTranslationShouldBeDone() {
-		Translator translator = new GoogleTranslator(splitter, fishMock());
+	public void translationShouldBeDone() {
+		Translator translator = new GoogleTranslator(splitter, translateMock());
 		assertEquals(translator.translate("fish", "en", "ru"), List.of("рыба"));
-	}
-
-	@Test
-	public void ruEnTranslationShouldBeDone() {
-		Translator translator = new GoogleTranslator(splitter, replaySongsMock());
 		assertEquals(translator.translate("повтор песни", "ru", "en"), List.of("replay", "songs"));
 	}
 
-	private static Translation translationMock(String res){
-		Translation translation = mock(Translation.class);
+	private Translate translateMock() {
+		Translate translate = mock(Translate.class);
+		enRuTranslations.forEach((word, transl) ->
+				addEnRuTranslation(translate, word, translationMock(transl)));
+		ruEnTranslations.forEach((word, transl) ->
+				addRuEnTranslation(translate, word, translationMock(transl)));
+		return translate;
+	}
+
+	private static Translation translationMock(String res) {
+		Translation translation = mock(Translation.class, RETURNS_DEEP_STUBS);
 		when(translation.getTranslatedText()).thenReturn(res);
 		return translation;
 	}
 
-	private Translate fishMock(){
-		Translate translate = mock(Translate.class);
-		when(translate.translate("fish", sourceLanguage("en"), targetLanguage("ru")))
-				.thenReturn(fish);
-		return translate;
+	private void addRuEnTranslation(Translate translate, String word, Translation res) {
+		when(translate.translate(word, sourceLanguage("ru"), targetLanguage("en")))
+				.thenReturn(res);
 	}
 
-	private Translate replaySongsMock(){
-		Translate translate = mock(Translate.class);
-		when(translate.translate("повтор", sourceLanguage("ru"), targetLanguage("en")))
-				.thenReturn(replay);
-		when(translate.translate("песни", sourceLanguage("ru"), targetLanguage("en")))
-				.thenReturn(song);
-
-		return translate;
+	private void addEnRuTranslation(Translate translate, String word, Translation res) {
+		when(translate.translate(word, sourceLanguage("en"), targetLanguage("ru")))
+				.thenReturn(res);
 	}
 }
